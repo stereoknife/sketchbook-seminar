f0c6c5538152e2b379c584ebe17f994e6685df6e
Sergiovan <sergiovanpas@gmail.com>
2023-05-09 15:44:05 +0200

    Fix linting and add a no-lint build to separate compiler errors from linter errors in CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
-  extends: ['next/core-web-vitals'/*, 'standard-with-typescript' */],
-  parserOptions: {
-    project: './tsconfig.json'
-  },
-  ignorePatterns: ['node_modules/']
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals'
+  ],
+  ignorePatterns: ['node_modules/', '.next/', 'out/', 'build/'],
+  rules: {
+    semi: ['error', 'never'],
+    quotes: ['error', 'single'],
+    '@typescript-eslint/no-empty-interface': ['error', {allowSingleExtends: true}]
+  }
 }
